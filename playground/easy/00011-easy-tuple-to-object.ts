f79ca726d059548a3feaa4f1f40c139f2bda4f04
Heecheon Park <heecheon@thesesamelab.com>
2024-09-26 14:36:17 +0900

    00011-easy-tuple-to-object ac

--- playground/easy/00011-easy-tuple-to-object.ts
+++ playground/easy/00011-easy-tuple-to-object.ts
@@ -20,7 +20,8 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly PropertyKey[]> = { [k in T[number]]: k }
+
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
