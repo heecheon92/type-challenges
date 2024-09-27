2f3a200a3e3d126bc9616c977319457b36d80caf
Heecheon Park <heecheon@thesesamelab.com>
2024-09-27 12:40:10 +0900

    00012-medium-chainable-options 풀이 추가 (이건 자력솔이 아니므로 복습 필수)

--- playground/medium/00012-medium-chainable-options.ts
+++ playground/medium/00012-medium-chainable-options.ts
@@ -39,9 +39,100 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+/**
+ * WA - 추후 Upsolve 필요.
+ */
+type __Chainable = {
+  option<T>(key: string, value: T): Chainable & Record<string, T>
+  get<T>(): T
+}
+
+/**
+ * https://github.com/type-challenges/type-challenges/issues/15337
+ * 감이 잘 안잡히므로, 빡고수의 풀이과정을 살펴보자.
+ *
+ * 이게 맨 처음 주어지는 베이스 타입인데 여기에서 "get"의 시그니쳐를 수정한다.
+ *
+ * type Chainable = {
+ *   option(key: string, value: any): any
+ *   get(): any
+ * }
+ * -------------------------------------------------------------
+ *
+ * 1. 아직 미정의 타입 R을 get의 반환으로 세팅
+ *
+ * type Chainable<R = object> = {
+ *   option(key: string, value: any): any
+ *   get(): R
+ * }
+ *
+ * -------------------------------------------------------------
+ *
+ * 2. option 함수 호출 이후 method chaining이 가능해야 하므로
+ * option의 반환 타입을 Chainable로 정의
+ *
+ * type Chainable<R = object> = {
+ *   option(key: string, value: any): Chainable
+ *   get(): R
+ * }
+ *
+ * -------------------------------------------------------------
+ *
+ * 3. 내가 시도 했을때 여기쯤에서 막혔음.... 아무튼 key-value (이하 Record)에서
+ * value의 타입은 다양할 수 있고, option 호출시마다 해당 타입정보를 유지해야 하므로
+ * option 함수의 시그니쳐를 변경
+ *
+ *    type Chainable<R = object> = {
+ *      option<K extends string, V>(key: K, value: V): Chainable<R & Record<K, V>>
+ *      get(): R
+ *    }
+ *
+ * -------------------------------------------------------------
+ *
+ * 4. 중복된 키가 들어오지 못하도록 설계해야 하는데 혼자 시도했을땐 위 3번에서 막혀서
+ * 시도해 보진 않았지만.
+ *
+ *     a. option<K extends string, V>     (key: K, value: V)
+ *     b. option<K extends keyof any, V>  (key: K extends keyof R ? never : K, value: V)
+ *
+ * a -> b로 시그니쳐 수정을 했다. 여기서 "key: K extends keyof R ? never : K" 이 부분이 중첩키를 방지하는
+ * 시그니쳐 인데.... option 함수의 generic placeholder는 "K extends string"에서 "K extends keyof any"로
+ * 바꾼건지는 잘 이해가 안간다.
+ *
+ *    type Chainable<R = object> = {
+ *      option<K extends keyof any, V>(key: K extends keyof R ? never : K, value: V): Chainable<R & Record<K, V>>
+ *      get(): R
+ *    }
+ *
+ * -------------------------------------------------------------
+ *
+ * 5. 정정 다른 코멘터들이 "K extends keyof any"로 변경 하는 과정은 필요 없다고 한다.
+ *
+ *    type Chainable<R = object> = {
+ *      option<K extends string, V>(key: K extends keyof R ? never : K, value: V): Chainable<R & Record<K, V>>
+ *      get(): R
+ *    }
+ *
+ * -------------------------------------------------------------
+ *
+ * 6. 중첩된 키라도 value의 타입이 다르면 마지막의 들어온 key-value로 덮어 쓴다.
+ *
+ *    type Chainable<R = object> = {
+ *      option<K extends string, V>(
+ *        key: Exclude<K, keyof R>,
+ *        value: V
+ *      ): Chainable<Omit<R, K> & Record<K, V>>;
+ *      get(): R;
+ *    };
+ *
+ */
+
+type Chainable<R = object> = {
+  option<K extends string, V>(
+    key: Exclude<K, keyof R>,
+    value: V
+  ): Chainable<Omit<R, K> & Record<K, V>>
+  get(): R
 }
 
 /* _____________ 테스트 케이스 _____________ */
