1099939554346ba5b472bc0c0be80f183a348154
Heecheon Park <heecheon@thesesamelab.com>
2024-09-30 08:43:39 +0900

    00191-medium-append-argument ac

--- playground/medium/00191-medium-append-argument.ts
+++ playground/medium/00191-medium-append-argument.ts
@@ -23,7 +23,7 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn extends (...args: any) => unknown, A> = Fn extends (...args: infer P) => infer R ? (...args: [...P, A]) => R : never
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
