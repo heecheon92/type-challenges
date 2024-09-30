e622859f3136920539a9d2970fb0e9942e8ef34a
Heecheon Park <heecheon@thesesamelab.com>
2024-09-30 14:07:51 +0900

    00529-medium-absolute ac

--- playground/medium/00529-medium-absolute.ts
+++ playground/medium/00529-medium-absolute.ts
@@ -20,7 +20,7 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type Absolute<T extends number | string | bigint> = any
+type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer S}` ? `${S}` : `${T}`
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
